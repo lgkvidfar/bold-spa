82d794f591cc7f881c15073181268a7f80ef3f87
Lukas Vidfar <lgkvidfar@gmail.com>
2024-05-18 14:52:46 +0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // base: 'bold-spa',
-})
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/bold-spa/' : '/',
+  };
+});
