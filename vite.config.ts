78544a6d653b0854a7db74fc151cd5d50a303b4b
Lukas Vidfar <lgkvidfar@gmail.com>
2024-05-23 18:04:39 +0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-    base: command === 'build' ? '/bold-spa/' : '/',
+    base: mode === 'production' ? '/bold-spa/' : '/',
   };
 });
